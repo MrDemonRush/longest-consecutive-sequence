46d1fba5216c6afea97eb2bfceb9b4bdf33b5679
MDR <mrdemonrush@gmail.com>
2018-10-07 15:53:27 +0300

    long-consec-seqv.1.0.0

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+  if(array.length == 0){
+    return 0;
+} else if(array.length == 1){
+    return 1;
+}
+
+
+ let count = 0;
+array.sort(function(a,b){return a-b;})
+const arr = [];
+
+for(let i = 0 ; i < array.length ; i++){
+  if(array[i]-array[i+1] !== -1 && array[i]!=array[i+1]){
+        count = 0;
+    }else if(array[i]-array[i+1] === -1){
+        count++; 
+        arr.push(count);
+    }
+} 
+if(arr.length === 0){
+  return 1;
+}
+const max = Math.max.apply(Math, arr);
+return max+1; 
 }
